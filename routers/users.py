b1c4a47df1453d9a4cfed70be2e15d34fd47d4be
hkd <1934456987@qq.com>
2026-05-01 16:14:17 +0800

    第二次提交： 用户表和用户token表完成，后端注册代码完成，随机生成token

---
+++ routers/users.py
@@ -0,0 +1,33 @@
+from fastapi import HTTPException
+
+from fastapi import APIRouter,Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from config.db_config import get_db
+from schemas.users import UserRequest
+from crud import users
+
+router=APIRouter(prefix="/api/users",tags=["users"])
+
+@router.post("/register")
+async def register(user_data:UserRequest,db:AsyncSession=Depends(get_db)):
+    #验证用户是否存在-》创建用户-》生成token-》响应结果
+    existing_user=await users.get_user_by_username(db,user_data.username)
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="用户已存在")
+    user=await users.create_user(db,user_data)
+    token=users.create_token(db,user.id)
+    return {
+        "code":200,
+        "message":"success",
+        "data":{
+            "token":token,
+            "userInfo":{
+                "id":user.id,
+                "username":user.username,
+                "avatar":user.avatar,
+                "bio":user.bio
+            }
+        }
+    }
